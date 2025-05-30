bd6102ca34d573dfd4990739956bd7d025694c23
DanielArellano31 <utm23090410@utma.edu.mx>
2025-05-29 23:38:12 -0600

    First

---
+++ Server/app/database/main.py
@@ -0,0 +1,11 @@
+import psycopg2
+
+class DataBase:
+    def __init__():
+        conexion = psycopg2.connect(
+            host ="postgres-a940e244.railway.internal",
+            database ="railway",
+            user ="postgress",
+            password ="fXWwjHXLjJIvQdsxmNyNnNnwWkjyodFw",
+            port ="5432"
+        )
